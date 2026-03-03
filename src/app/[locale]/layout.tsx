06835524b936c5dd33d747d632c09afc526562e3
zefparis <ben.barere@gmail.com>
2026-03-03 21:01:15 +0100

    f

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages, setRequestLocale } from 'next-intl/server';
 import { notFound } from 'next/navigation';
@@ -65,6 +66,11 @@ export default async function LocaleLayout({ children, params }: Props) {
       <Header />
       <main className="min-h-screen pt-16 md:pt-20">{children}</main>
       <Footer />
+      <Script
+        src="https://hcs-widget-mvp.vercel.app/widget/v3/hcs-widget.js"
+        data-widget="wid_252792d76ceaa21f2d263aab"
+        strategy="afterInteractive"
+      />
     </NextIntlClientProvider>
   );
 }
